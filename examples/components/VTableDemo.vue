2b17e78456e9ad53d316e27f4e1125f6252e9d1b
peter-wulin <1434071456@qq.com>
2020-05-09 09:18:04 +0800

    checkbox test

--- examples/components/VTableDemo.vue
+++ examples/components/VTableDemo.vue
@@ -4,48 +4,51 @@
  * @Description: file content
  -->
 <template>
-  <VTable :columns="columns" :data="tableData"></VTable>
+  <VTable @select="bindSelect" :columns="columns" :data="tableData"></VTable>
 </template>
 
 <script>
 export default {
-  name: "vtable-demo",
+  name: 'vtable-demo',
   data() {
     return {
       columns: [
         {
-          label: "日期",
-          prop: "date",
-          formatter: value => {
-            return value;
-          }
+          label: '日期',
+          prop: 'date',
+          formatter: (value) => {
+            return value
+          },
         },
-        { label: "姓名", prop: "name", tips: "hello" },
-        { label: "地址", prop: "address" }
+        { label: '姓名', prop: 'name', tips: 'hello' },
+        { label: '地址', prop: 'address' },
       ],
       tableData: [
         {
-          date: "2016-05-02",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄"
+          date: '2016-05-02',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄',
         },
         {
-          date: "2016-05-04",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1517 弄"
+          date: '2016-05-04',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1517 弄',
         },
         {
-          date: "2016-05-01",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1519 弄"
+          date: '2016-05-01',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1519 弄',
         },
         {
-          date: "2016-05-03",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1516 弄"
-        }
-      ]
-    };
-  }
-};
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1516 弄',
+        },
+      ],
+    }
+  },
+  methods: {
+    bindSelect() {},
+  },
+}
 </script>
