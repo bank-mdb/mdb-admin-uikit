85f373d190d4d5db1730d17cf95a6a454e58a952
zhang.ning <zhang.ning@mdb-fintech.com>
2020-05-24 14:24:53 +0800

    支持自定义组件在使用dy-form组件的地方注册为局部组件

--- packages/DyForm/src/dynamicCol.js
+++ packages/DyForm/src/dynamicCol.js
@@ -9,6 +9,15 @@ export default {
       required: true
     }
   },
+  beforeCreate(){
+    let vm = this.$parent;
+    // 拿到在dy-form组件中的局部组件
+    while(vm && vm.$options._componentTag !== "dy-form") {
+      vm = vm.$parent;
+    }
+    if(!vm) return;
+    Object.assign(this.$options.components, vm.$options.components)
+  },
   render: function(h) {
     let self = this;
     return h(
